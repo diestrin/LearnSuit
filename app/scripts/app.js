a5813d1188d4611520418af9c79ffc496cb14c13
Diego Barahona <diestrin@gmail.com>
2012-10-05 00:26:56 -0600

    Update to the new custom router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,23 +2,102 @@
 
 // App Module
 
-angular.module("learningcenter", [])
-	.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider){
-		//$locationProvider.html5Mode(true);
-		$routeProvider
-			.when("/login", {
-				templateUrl: "views/login.html",
-				controller: loginController
-			})
-			.when("/!/:path", {
-				templateUrl: "views/home.html",
-				controller: homeController
-			})
-			.when("/!", {
-				templateUrl: "views/home.html",
-				controller: homeController
-			})
-			.otherwise({
-				redirectTo: "/login"
-			});
-	}]);
+angular.module("learningcenter", [], function($provide){
+	$provide.factory("bookLibrary", ["$http", function($http){
+			var libraryResources;
+
+			function fetchResources(callback){
+				$http({
+					method: "jsonp",
+					url: 'http://localhost/getFolders.php?callback=JSON_CALLBACK'
+				})
+				.success(function(data, status){
+					libraryResources = data;
+					callback();
+				});
+			}
+
+			function initLibrary(callback){
+				if(!libraryResources){
+					fetchResources(callback);
+				}else{
+					callback();
+				}
+			}
+
+			function Library(callback){
+				initLibrary(function(){
+					callback.apply(Library, []);
+				});
+			}
+
+			Library.get = function(path){
+				var output = [];
+
+				function recursive(pathSection, paths, length){
+					angular.forEach(libraryResources, function(resource, index){
+						if("url" in resource && resource.url === pathSection){
+							if(length > 0){
+								recursive(paths
+									.slice(0, paths.length - length - 1)
+									.join("/"), paths, length - 1);
+							}else{
+								angular.forEach(resource.items, function(propery){
+									output.push(removeItems(propery));
+								});
+							}
+						}
+
+					});
+				}
+
+				function removeItems(i){
+					var o = {},
+						item;
+
+					for(item in i)
+						if(item !== "items")
+							o[item] = i[item];
+
+					return o;
+				}
+
+				if(path){
+					path = path.replace(/\/\w+?\.?\w+?$/,'').replace(/\/$/);
+					path = path.split("/");
+
+					recursive(path[0], path, path.length-1);
+				}else{
+					angular.forEach(libraryResources, function(resource){
+						output.push(removeItems(resource));
+					});
+				}
+
+				return output;
+			}
+
+			Library.resources;
+			Library.currentPath = "";
+	
+			return Library;
+		}]);
+})
+.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider){
+	//$locationProvider.html5Mode(true);
+	$routeProvider
+		.when("/login", {
+			templateUrl: "views/login.html",
+			controller: loginController
+		})
+		.when("/!/:path", {
+			templateUrl: "views/home.html",
+			controller: homeController
+		})
+		.when("/!", {
+			templateUrl: "views/home.html",
+			controller: homeController
+		})
+		.otherwise({
+			redirectTo: "/login"
+		});
+}]);
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -19,23 +19,26 @@ function loginController ($scope, $http, $location){
 	$scope.error = false;
 }
 
-function homeController ($scope, $routeParams, $http, $location, $rootScope){
+function homeController ($scope, $routeParams, $http, $location, bookLibrary){
 	if(localStorage.getItem("usrInfo") !== "dGVzdH4vfnRlc3Q=") 
 		$location.url("/login");
 
-	function win(data){
-		$scope.fields = data;
-	}
+	var query = !!$routeParams.path && $routeParams.path.replace(/\w+\.\w+$/, '') ? 
+		"path=" + $routeParams.path.replace(/\/\w+\.\w+$/, '') + "&" : "";
+
+	bookLibrary(function(){
+		$scope.fields = this.get($routeParams.path);
+	});
 	
-	function isSelected(itemId){
-		return itemId === $routeParams.path && isBook(itemId) ? "active" : "";
+	function isSelected(itemPath){
+		return itemPath === $routeParams.path && isBook(itemPath) ? "active" : "";
 	}
 
-	function isBook(itemId){
+	function isBook(itemPath){
 		var is = false;
 
 		angular.forEach($scope.fields, function(book){
-			if(book.id === itemId && book.type === "file")
+			if(book.url === itemPath && book.type === "file")
 				return is = true;
 		});
 
@@ -46,46 +49,19 @@ function homeController ($scope, $routeParams, $http, $location, $rootScope){
 		var selected;
 
 		angular.forEach($scope.fields, function(book){
-			if(book.id === $routeParams.path)
+			if(book.url === $routeParams.path)
 				return selected = book;
 		});
 
 		return selected
 	}
 
-	if(!$rootScope.fields){
-		// $http({
-		// 	method: "jsonp",
-		// 	url: 'http://localhost/getFolder.php?callback=JSON_CALLBACK&' + $routeParams.path
-		// })
-		// .success(function(data, status){
-		// 	win(data);
-		// })
-		// .error(function(data, status){
-		// 	win(data);
-		// });
-		$http({
-			method: "jsonp",
-			url: 'http://localhost/getFolders.php?callback=JSON_CALLBACK'
-		})
-		.success(function(data, status){
-			$rootScope.fields = data;
-			$scope.fields = $rootScope.fields;
-		})
-		.error(function(data, status){
-			$rootScope.fields = data;
-			$scope.fields = $rootScope.fields;
-		});
-	}
-
-	$scope.fields = $rootScope.fields;
-
 	$scope.selected = getSelected();
 	$scope.isSelected = isSelected;
 	$scope.isBook = isBook;
 	$scope.getSelected = getSelected;
 
-	if(!!$scope.selected && !isBook($scope.selected.id)){
+	if(!!$scope.selected && !isBook($scope.selected.url)){
 		$scope.fields = $scope.selected.items;
 	}
 }
